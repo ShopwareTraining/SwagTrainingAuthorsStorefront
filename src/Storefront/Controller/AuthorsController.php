4abae79f3586babd2a9027f11a32107eccd9106e
jisse Reitsma <jisse@yireo.com>
2021-07-06 16:49:57 +0200

    Adding all files to git via Yireo Command

---
+++ src/Storefront/Controller/AuthorsController.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace SwagTraining\AuthorsStorefront\Storefront\Controller;
+
+use Shopware\Core\Framework\Context;
+use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\ContainsFilter;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
+use Shopware\Core\Framework\Routing\Annotation\RouteScope;
+use Shopware\Core\Framework\Routing\Annotation\Since;
+use Shopware\Core\System\SystemConfig\SystemConfigService;
+use Shopware\Storefront\Controller\StorefrontController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @RouteScope(scopes={"storefront"})
+ */
+class AuthorsController extends StorefrontController
+{
+    private EntityRepositoryInterface $productRepository;
+
+    /**
+     * AuthorsController constructor.
+     * @param EntityRepositoryInterface $productRepository
+     */
+    public function __construct(EntityRepositoryInterface $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+    /**
+     * @Since("6.4.0.0")
+     * @Route("/authors", name="frontend.swag-training-authors.authors", methods={"GET"}, defaults={"XmlHttpRequest"=true})
+     */
+    public function getData(Request $request, Context $context): Response
+    {
+        $criteria = new Criteria;
+        $criteria->addFilter(new EqualsFilter('parentId', null));
+        $criteria->addFilter(new ContainsFilter('name', 'M'));
+        $criteria->setLimit(3);
+
+        $products = $this->productRepository->search($criteria, $context);
+        return $this->renderStorefront('@SwagTrainingAuthorsStorefront/storefront/page/content/authors.html.twig', [
+            'products' => $products
+        ]);
+    }
+}
---
+++ src/SwagTrainingAuthorsStorefront.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace SwagTraining\AuthorsStorefront;
+
+use Shopware\Core\Framework\Plugin;
+
+class SwagTrainingAuthorsStorefront extends Plugin
+{
+}
