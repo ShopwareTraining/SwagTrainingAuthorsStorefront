6a9d8a0b8c02e7d74cdf1712cbbc71e948dc5abf
jisse Reitsma <jisse@yireo.com>
2021-07-07 16:58:49 +0200

    Adding all files to git via Yireo Command

--- src/Storefront/Controller/AuthorsController.php
+++ src/Storefront/Controller/AuthorsController.php
@@ -5,11 +5,8 @@ namespace SwagTraining\AuthorsStorefront\Storefront\Controller;
 use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
 use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\ContainsFilter;
-use Shopware\Core\Framework\DataAbstractionLayer\Search\Filter\EqualsFilter;
 use Shopware\Core\Framework\Routing\Annotation\RouteScope;
 use Shopware\Core\Framework\Routing\Annotation\Since;
-use Shopware\Core\System\SystemConfig\SystemConfigService;
 use Shopware\Storefront\Controller\StorefrontController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,30 +17,31 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class AuthorsController extends StorefrontController
 {
-    private EntityRepositoryInterface $productRepository;
+    private EntityRepositoryInterface $authorRepository;
 
-    public function __construct(EntityRepositoryInterface $productRepository)
+    /**
+     * AuthorsController constructor.
+     * @param EntityRepositoryInterface $authorRepository
+     */
+    public function __construct(EntityRepositoryInterface $authorRepository)
     {
-        $this->productRepository = $productRepository;
+        $this->authorRepository = $authorRepository;
     }
 
     /**
      * @Since("6.4.0.0")
-     * @Route("/authors", name="frontend.swag-training.authors", methods={"GET"},
-     *                    defaults={"XmlHttpRequest"=true})
+     * @Route("/swag-training/authors", name="frontend.swag-training.authors", methods={"GET"}, defaults={"XmlHttpRequest"=true})
      */
     public function getData(Request $request, Context $context): Response
     {
         $criteria = new Criteria;
-        $criteria->addFilter(new EqualsFilter('parentId', null));
-        $criteria->addFilter(new ContainsFilter('name', 'M'));
         $criteria->setLimit(3);
 
-        $products = $this->productRepository->search($criteria, $context);
+        $authors = $this->authorRepository->search($criteria, $context);
 
         return $this->renderStorefront(
             '@SwagTrainingAuthorsStorefront/storefront/page/content/authors.html.twig', [
-            'products' => $products,
+            'authors' => $authors,
         ]);
     }
 }
